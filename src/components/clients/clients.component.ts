f2795320c7be9327cacf14ad746bd9260695edf5
Muhammadkhizar <khizeraziz786@gmail.com>
2023-07-08 19:30:19 +0500

    work on ui

--- src/components/clients/clients.component.ts
+++ src/components/clients/clients.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.css']
+  styleUrls: ['./clients.component.scss']
 })
 export class ClientsComponent implements OnInit {
+  @Input() numVisible = 5;
+  public products = [
+    {
+        id: '1000',
+        code: 'f230fh0g3',
+        image: 'assets/images/kfc-case-logo.svg',
+        description: 'How we built a scalable and robust cloud-based food delivery application for the world’s most popular fast-food chain - KFC. The result? Massive improvement in the app’s conversion rate and number of downloads.',
+        states: '28%',
+        statesText: 'Increase invconversion rate',
+        states1: '2 Million',
+        statesText1: 'Mobile<br>app downloads',
+        stateCaseStudy: 'Read Case Study',
+        image1: 'assets/images/kfc-port-banner.png',
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      image: 'assets/images/ikea-case-logo.svg',
+      description: 'How we built a cloud-based ERP solution for the world’s largest furniture retailer - IKEA. The result? The retail outlet touts the solution as its biggest source of RoI measurement. The solution is also getting expanded in the brand’s multiple other retail outlets.',
+      states: '7+',
+      statesText: 'IKEA stores adopting the solution',
+      stateCaseStudy: 'View Case Study',
+      image1: 'assets/images/ikea-port-banner.png',
+    },
+   
+];
+public responsiveOptions!: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+    this.responsiveOptions = [
+      {
+          breakpoint: '1199px',
+          numVisible: 1,
+          numScroll: 1
+      },
+      {
+          breakpoint: '991px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '767px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
   }
+  getSeverity(status: string) {
+    switch (status) {
+        case 'INSTOCK':
+            return 'success';
+        case 'LOWSTOCK':
+            return 'warning';
+        case 'OUTOFSTOCK':
+            return 'danger';
+            default: return '';
+    }
+}
 
 }
--- src/components/projects/projects.component.ts
+++ src/components/projects/projects.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  @Input() numVisible = 5;
+  public products = [
+    {
+        id: '1000',
+        code: 'f230fh0g3',
+        name: '01. Analyzing existing IT infrastructure',
+        description: 'We kickstart the project by doing an in-depth analysis of your business processes and software infrastructure. The objective behind the analysis is to find every stumbling block that can prevent the successful execution of the DevOps strategy.',
+        image: 'assets/images/devops-detail-img1.png',
+    },
+    {
+        id: '1001',
+        code: 'nvklal433',
+        name: '02. Developing a tailor-made solution',
+        description: 'Our team suggests a tailor-made DevOps solution considering your current IT issues, giving you the prospect on the team size, budget, and timeframe before implementing the solution. Our team also lays a complete roadmap for the<br> perfect execution of the solution.',
+        image: 'assets/images/devops-detail-img2.png',
+    },
+    {
+        id: '1002',
+        code: 'zz21cz3c1',
+        name: '03. Deploying the best solution',
+        description: 'Using the industry-best practices and state-of-the-art tools, our DevOps engineers set up a CI/CD pipeline that is in line with your business objectives and organize a clear release management process while tackling all problems related to your software infrastructure efficiently.',
+        image: 'assets/images/devops-detail-img3.png',
+    },
+    {
+        id: '1003',
+        code: '244wgerg2',
+        name: '04. Providing 24/7 support',
+        description: 'As one of the fastest growing cloud and DevOps companies, we offer 24/7 end-to-end support to your infrastructures, workloads, and operations. We also help you with real-time incident tracking and resolving support cases, enabling you to take the right actions faster.',
+        image: 'assets/images/devops-detail-img4.png',
+    },
+   
+];
+public responsiveOptions!: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+    this.responsiveOptions = [
+      {
+          breakpoint: '1199px',
+          numVisible: 1,
+          numScroll: 1
+      },
+      {
+          breakpoint: '991px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '767px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
   }
-
+  getSeverity(status: string) {
+    switch (status) {
+        case 'INSTOCK':
+            return 'success';
+        case 'LOWSTOCK':
+            return 'warning';
+        case 'OUTOFSTOCK':
+            return 'danger';
+            default: return '';
+    }
+}
 }
