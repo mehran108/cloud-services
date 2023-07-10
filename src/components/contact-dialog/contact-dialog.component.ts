b969648f43f58c93695b5a69f051297daf842879
Muhammadkhizar <khizeraziz786@gmail.com>
2023-07-10 23:48:55 +0500

    work on ui

--- src/components/contact-dialog/contact-dialog.component.ts
+++ src/components/contact-dialog/contact-dialog.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-contact-dialog',
   templateUrl: './contact-dialog.component.html',
-  styleUrls: ['./contact-dialog.component.css']
+  styleUrls: ['./contact-dialog.component.scss']
 })
 export class ContactDialogComponent implements OnInit {
   @Input() visible: boolean = false;
